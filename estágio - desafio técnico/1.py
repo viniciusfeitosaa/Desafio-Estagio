23c71b622ed8107cd86215ccd90e31984b0a64b9
Vinícius Feitosa <83141381+viniciusfeitosaa@users.noreply.github.com>
2024-09-03 16:14:25 -0300

    Delete estágio - desafio técnico directory

--- estágio - desafio técnico/1.py
+++
@@ -1,9 +0,0 @@
-INDICE = 13
-SOMA = 0
-K = 0
-
-while K < INDICE:
-    K += 1
-    SOMA += K
-
-print(SOMA)
--- estágio - desafio técnico/5.py
+++
@@ -1,8 +0,0 @@
-def inverter_string(s):
-    invertida = ""
-    for char in s:
-        invertida = char + invertida
-    return invertida
-
-string = input("Informe uma string: ")
-print(f"String invertida: {inverter_string(string)}")
